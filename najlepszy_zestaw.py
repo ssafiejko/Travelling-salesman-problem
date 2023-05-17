960f5f8a998c7626e4623206670554e9c9c2a820
piotr-kosakowski <piotrkosakowski@icloud.com>
2023-05-17 18:15:38 +0200

    funkcja parametry

--- najlepszy_zestaw.py
+++ najlepszy_zestaw.py
@@ -1,8 +1,5 @@
-# from wbtest import *
 import pandas as pd
-# import random
 
-# random.seed(2137)
 
 
 def find_best_params(results_df:pd.DataFrame, i=10):
@@ -16,13 +13,10 @@ def find_best_params(results_df:pd.DataFrame, i=10):
     df = df.fillna('-')
     df = df.drop(['path', 'time'], axis=1)
 
-    # df['min'] = df.groupby(['file_name'])['function_result'].transform('min')
     df['scaled_res'] = df.groupby('file_name')['function_result'].transform(lambda x: (x - x.min()) / (x.max() - x.min()))
 
     
-    # df['min_scaled'] = df.groupby(['file_name'])['scaled_res'].transform('min')
 
-    # df = df.reset_index(drop=True)
 
     df['mean_scaled_res'] = df.groupby(['prob_function', 'prob_parameter', 'temp_function',
                         'cool_parameter', 'T_init'])['scaled_res'].transform('mean')
@@ -33,24 +27,15 @@ def find_best_params(results_df:pd.DataFrame, i=10):
     
     df_dropped_duplicates = df_dropped_duplicates.sort_values(by=['mean_scaled_res'])
 
-    # print(df_sorted.shape)
-    # print(df_sorted.head(100))
-    # print(df_dropped_duplicates.iloc)
-    # print(df_dropped_duplicates.shape)
-    # print(df.head(15))
+
     return df_dropped_duplicates.head(i)
-    # df.to_csv('elo.csv')
 
 
 def main():
     pd.set_option('display.max_rows', None)
-    # pd.set_option('display.max_columns', None)  
     df = pd.read_csv('test.csv')
 
     print(find_best_params(df,5))
-    # print(df)
-
-
 
 
 if __name__ == '__main__':
